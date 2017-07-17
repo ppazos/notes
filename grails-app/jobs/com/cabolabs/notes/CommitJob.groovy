1f7a8e8cecb204681660ea628c2084b7f17b54ee
Pablo Pazos <pablo.pazos@cabolabs.com>
2017-07-17 15:09:09 -0300

    commit job now working

--- grails-app/jobs/com/cabolabs/notes/CommitJob.groovy
+++ grails-app/jobs/com/cabolabs/notes/CommitJob.groovy
@@ -1,18 +1,26 @@
 package com.cabolabs.notes
 
 class CommitJob {
-    static triggers = {
+   static triggers = {
       simple repeatInterval: 5000l // execute job once in 5 seconds
-    }
+   }
 
-    def execute()
-    {
-       println "job"
-        // execute job
-        String PS = System.getProperty("file.separator")
-        def outbox = new File("documents" +PS+ "pending")
-        outbox.eachFile { f ->
-           println "File to commit "+ f.name
-        }
-    }
+   def execute()
+   {
+      println "job"
+       // execute job
+      String PS  = System.getProperty("file.separator")
+      def outbox = new File("documents" +PS+ "pending")
+      def sent   = new File("documents" +PS+ "sent")
+
+      outbox.eachFile { f ->
+         println "File to commit "+ f.name
+
+         
+         // TODO: commit, the file already has the versions envelope
+
+         // move
+         f.renameTo(new File(sent, f.name))
+      }
+   }
 }
