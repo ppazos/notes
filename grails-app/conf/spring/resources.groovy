1ca110cb8bdf8a988a3eea02e63ac7bf60365c68
Pablo Pazos <pablo.pazos@cabolabs.com>
2017-07-28 06:18:10 -0300

    set default locale EN

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,7 +1,12 @@
 import com.cabolabs.security.UserPasswordEncoderListener
 import com.cabolabs.security.UserPasswordEncoderListener
+import org.springframework.web.servlet.i18n.SessionLocaleResolver
+
 // Place your Spring DSL code here
 beans = {
+	localeResolver(SessionLocaleResolver) { 
+        defaultLocale = new java.util.Locale('en') // Default locale
+    }
     userPasswordEncoderListener(UserPasswordEncoderListener, ref('hibernateDatastore'))
     userPasswordEncoderListener(UserPasswordEncoderListener, ref('hibernateDatastore'))
 }
